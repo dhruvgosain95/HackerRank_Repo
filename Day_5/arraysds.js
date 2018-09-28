7b543316ab2df41e386774d4b1bf9923f5d119df
Dhruv Gosain <dhruv.g@techaspect.com>
2018-09-28 10:58:30 +0530

    Added Source Code to Arrays DS(js).

---
+++ Day_5/arraysds.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the reverseArray function below.
+function reverseArray(X) {
+    
+    
+    var Y= [X.length];
+        var Z=X.length-1;
+        for(var i=0;i<X.length;i++){
+            Y[Z]=X[i];
+            Z--;
+        }
+        return Y;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
