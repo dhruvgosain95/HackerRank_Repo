2da4a6db61780770dc1e659038f4ab07c1d66e9c
Dhruv Gosain <dhruv.g@techaspect.com>
2018-10-04 15:32:47 +0530

    Added Source Code to Kangaroo(js).

---
+++ Day_10/kangaroo.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the kangaroo function below.
+function kangaroo(kang1_location, kang1_speed, kang2_location, kang2_speed) {
+   var output="NO";
+   var x=kang1_location;
+  var y=kang2_location;
+while(x>=0&&y>=0&&x<=100000000&&y<=100000000){
+    if(x==y){
+        output="YES";
+        break;
+    }
+    x=x+kang1_speed;
+    y=y+kang2_speed; 
+}
+return output;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const x1V1X2V2 = readLine().split(' ');
+
+    const x1 = parseInt(x1V1X2V2[0], 10);
+
+    const v1 = parseInt(x1V1X2V2[1], 10);
+
+    const x2 = parseInt(x1V1X2V2[2], 10);
+
+    const v2 = parseInt(x1V1X2V2[3], 10);
+
+    let result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
