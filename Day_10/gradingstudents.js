91b6b1fcf2083df722929c89600d018ec5074d0e
Dhruv Gosain <dhruv.g@techaspect.com>
2018-10-04 15:43:55 +0530

    Added Source Code to Grading Students(js).

---
+++ Day_10/gradingstudents.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the gradingStudents function below.
+ */
+function gradingStudents(grades) {
+     var index,temp=0;
+     for(index=0;index<grades.length;index++) {
+        if(grades[index]>=38) {
+          temp=grades[index];
+            while( temp % 5! = 0 ) {
+                temp++;
+            }
+            if((temp-(grades[index])) < 3 ) {
+                grades[index]= temp;
+            }
+        }
+    }
+               return grades;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let grades = [];
+
+    for (let gradesItr = 0; gradesItr < n; gradesItr++) {
+        const gradesItem = parseInt(readLine(), 10);
+        grades.push(gradesItem);
+    }
+
+    let result = gradingStudents(grades);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
