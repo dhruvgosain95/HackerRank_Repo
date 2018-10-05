2b218009bf2f04dddf9d8be445d998773bd5a54e
Dhruv Gosain <dhruv.g@techaspect.com>
2018-10-05 13:48:57 +0530

    Added Source Code to Compare Triplets\(js).

---
+++ Day_10/comparetriplets.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    var result=[];
+    result[0]=0; 
+    result[1]=0;
+    for( var index=0;index<=a.length;index++) {
+                if(a[index]>b[index]) {
+                        result[0]++;
+                }
+                 if(a[index]<b[index]) {
+                        result[1]++;
+                }
+   }
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
