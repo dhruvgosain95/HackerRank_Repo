3632e49b3d7f09b7542c1e6ad776933162f14415
Dhruv Gosain <dhruv.g@tadigital.com>
2018-10-09 18:16:55 +0530

    Added Source Code for Camel Case.

---
+++ Day_13/camelcase.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the camelcase function below.
+function camelcase(s) {
+ const line = s;
+    const words = line.split(/[A-Z]/g);
+    return words.length;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = camelcase(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
