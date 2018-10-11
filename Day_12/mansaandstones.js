78ea751fe67253d6032d1a5f2e145ad20933b711
Dhruv Gosain <dhruv.g@tadigital.com>
2018-10-11 10:50:47 +0530

    Added Source Code for Mansa and Stones.

---
+++ Day_12/mansaandstones.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the stones function below.
+function stones(n, a, b) {
+    
+     var first=0;
+        var sum=0;
+        var ar=[];
+        first=(n-1)*Math.min(a,b);
+        ar.push(first);
+        while(sum+first<(n-1)*Math.max(a,b))
+        {
+            sum+=Math.abs(b-a);
+           ar.push(first+sum);
+        }
+      
+        return ar;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const n = parseInt(readLine(), 10);
+
+        const a = parseInt(readLine(), 10);
+
+        const b = parseInt(readLine(), 10);
+
+        let result = stones(n, a, b);
+
+        ws.write(result.join(" ") + "\n");
+    }
+
+    ws.end();
+}
